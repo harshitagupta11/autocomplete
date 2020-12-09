f7d73d0f0ef49f4e15bc3c4f223902940fb98d58
harshitagupta11 <guptaharshita99@gmail.com>
2020-12-09 11:32:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import countries from './countries.json';
+ const countrie =countries.map(country=>{
+  country.Country=country.Country.toLowerCase()
+   return country
+})
+//const countries = fs.readFile();
+//console.log(countrie[0]);
+const Auto = () => {
+  const [display, setDisplay] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [search, setSearch] = useState("");
+  const wrapperRef = useRef(null);
+  useEffect(() => {
+    setOptions(countrie);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const handleClickOutside = event => {
+    const { current: wrap } = wrapperRef;
+    if (wrap && !wrap.contains(event.target)) {
+      setDisplay(false);
+    }
+  };
+
+  const updatePokeDex = poke => {
+    setSearch(poke);
+    setDisplay(false);
+  };
+
+  return (
+    <div ref={wrapperRef} className="flex-container flex-column pos-rel">
+      <input
+        id="auto"
+        onClick={() => setDisplay(!display)}
+        placeholder="Type to search"
+        value={search}
+        onChange={event => setSearch(event.target.value)}
+      />
+      {display && (
+        <div className="autoContainer">
+          {options
+            .filter(({Country}) => Country.indexOf(search.toLowerCase()) > -1)
+            .map((value, i) => {
+              return (
+                <div
+                  onClick={() => updatePokeDex(value.Country)}
+                  className="option"
+                  key={i}
+                  tabIndex="0"
+                >
+                  <span>{value.Country}</span>
+                  
+                </div>
+              );
+            })}
+        </div>
+      )}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Custom AutoComplete React</h1>
+      
+      <div className="auto-container">
+        <Auto />
+      </div>
     </div>
   );
 }
